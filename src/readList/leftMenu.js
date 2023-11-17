754091ce9623ace73b32e32bbcc42141bd79b868
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2023-11-18 02:23:14 +0300

    Switching to slices

--- src/readList/leftMenu.js
+++ src/readList/leftMenu.js
@@ -1,65 +1,48 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import {openSeriesList} from '../redux/actionCreators.js'
 import Nav from 'react-bootstrap/Nav'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import {
     openBookList, openAuthorList
 } from './books/booksSlice.js'
+import { useDispatch } from 'react-redux'
 
-class LeftMenu extends React.Component{
 
-    render(){
+export default function LeftMenu(){
+    const dispatch = useDispatch();
 
-        return (
-            <Row><Col>
-            <Row>
-                <Col>
-                <div class="mt-4 mb-4"
-                >BROWSE</div>
-                </Col>
-            </Row>
-
-            <Row>
-                <Col>
-            <Nav defaultActiveKey="1" className="flex-column" variant="pills">
-                <Nav.Item>
-                    <Nav.Link 
-                        eventKey="1"
-                        onSelect={()=>{
-                            this.props.openBookList();
-                        }}
-                    >Book List</Nav.Link>
-                </Nav.Item>
-                {/* <Nav.Item>
-                    <Nav.Link 
-                        eventKey="2"
-                        onSelect={()=>{
-                            this.props.openSeriesList()
-                        }}
-                    >Series List</Nav.Link>
-                </Nav.Item> */}
-                <Nav.Item>
-                    <Nav.Link 
-                        eventKey="3"
-                        onSelect={()=>{
-                            this.props.openAuthorList()
-                        }}
-                    >Author List</Nav.Link>
-                </Nav.Item>
-            </Nav>
+    return (
+        <Row><Col>
+        <Row>
+            <Col>
+            <div class="mt-4 mb-4"
+            >BROWSE</div>
             </Col>
-            </Row>
-            </Col></Row>
-            
-        )
-    }
+        </Row>
+
+        <Row>
+            <Col>
+        <Nav defaultActiveKey="1" className="flex-column" variant="pills">
+            <Nav.Item>
+                <Nav.Link 
+                    eventKey="1"
+                    onSelect={()=>{
+                        dispatch(openBookList());
+                    }}
+                >Book List</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link 
+                    eventKey="3"
+                    onSelect={()=>{
+                        dispatch(openAuthorList());
+                    }}
+                >Author List</Nav.Link>
+            </Nav.Item>
+        </Nav>
+        </Col>
+        </Row>
+        </Col></Row>
+        
+    )
 }
-
-// export default LeftMenu;
-
-export default connect(
-	null,
-	{ openSeriesList, openBookList,openAuthorList}
-  )(LeftMenu)
--- src/readList/readList.js
+++ src/readList/readList.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import ListPanel from './books/listPanel.js'
-import SettingsPanel from '../settingsPanel.js'
 import LeftMenu from './leftMenu.js' 
 import Book from './books/book.js'
 import BookAdd from './books/add.js'
@@ -10,80 +7,42 @@ import BookEdit from './books/edit.js'
 import AuthorList from './authors/list.js'
 import Author from './authors/view.js'
 import AuthorAdd from './authors/add.js'
-import { connect } from 'react-redux'
-import * as panels from '../panels.js'
 import * as bookForms from './books/forms.js'
-import {openBookAdd, openBook} from './books/booksSlice.js'
-import {openSignIn} from '../displayAreaSlice.js'
+import { useSelector } from 'react-redux'
 
 
-class ReadList extends React.Component{
-    constructor(props){
-		super(props);
-		this.state={}
+export default function ReadList(){
+	const store={
+		bookForm: useSelector(state=>state.booksReducer.form)
     }
-    
-    render(){
-        const panel = -1;
-		let displayPanel;
-		if (this.props.store.bookForm===bookForms.SHOW_LIST){
-			displayPanel= (<ListPanel/>)
-		} else if (this.props.store.bookForm===bookForms.SHOW_BOOK_ADD){
-			displayPanel=(<BookAdd/>)			
-		} else if (panel===4){
-			displayPanel=(
-				<SettingsPanel
-				user = {this.state.user}
-				/>
-			)
-        } else if (this.props.store.bookForm===bookForms.SHOW_BOOK){
-			displayPanel=(<Book/>)
-		} else if (this.props.store.bookForm===bookForms.SHOW_BOOK_UPDATE){
-			displayPanel=(<BookEdit/>)
-		} else if (this.props.store.bookForm===bookForms.SHOW_AUTHOR_LIST){
-			displayPanel=(<AuthorList/>)
-		} else if (this.props.store.bookForm===bookForms.SHOW_AUTHOR){
-			displayPanel=(<Author/>)
-		}else if (this.props.store.bookForm===bookForms.SHOW_AUTHOR_ADD){
-			displayPanel=(<AuthorAdd/>)
-		}
 
+	let displayPanel;
+	if (store.bookForm===bookForms.SHOW_LIST){
+		displayPanel= (<ListPanel/>)
+	} else if (store.bookForm===bookForms.SHOW_BOOK_ADD){
+		displayPanel=(<BookAdd/>)			
+	} else if (store.bookForm===bookForms.SHOW_BOOK){
+		displayPanel=(<Book/>)
+	} else if (store.bookForm===bookForms.SHOW_BOOK_UPDATE){
+		displayPanel=(<BookEdit/>)
+	} else if (store.bookForm===bookForms.SHOW_AUTHOR_LIST){
+		displayPanel=(<AuthorList/>)
+	} else if (store.bookForm===bookForms.SHOW_AUTHOR){
+		displayPanel=(<Author/>)
+	}else if (store.bookForm===bookForms.SHOW_AUTHOR_ADD){
+		displayPanel=(<AuthorAdd/>)
+	}
 
-        let leftMenu;
-		if (this.props.store.panelType !== panels.SIGN_IN) {
-			leftMenu = (<LeftMenu/>)
-		}
+	let leftMenu = (<LeftMenu/>);
 
-        return(
-            <Row>
-                <Col bsPrefix="col-md-2 pr-5">
-                    {leftMenu}
-                </Col>
-                <Col bsPrefix="col-md-10 pr-5">
-                    {displayPanel}
-                </Col>	
-            </Row>
-        )
-    }
+	return(
+		<div class="row">
+			<div class="col-md-2 pr-5">
+				{leftMenu}
+			</div>
+			<div class="col-md-10 pr-5">
+				{displayPanel}
+			</div>	
+		</div>
+	)
 }
-
-const mapStatetoProps = (state) => {
-	return {
-		store: {
-			JWT: state.loginReducer.JWT,
-			listId: state.listsReducer.listId,
-			bookId: state.listsReducer.book.bookId,
-			listType: state.listsReducer.listType,
-			bookForm: state.booksReducer.form
-		}
-	};
-}
-
-export default connect(
-    mapStatetoProps,
-    {
-        openSignIn,
-        openBook,
-		openBookAdd
-    }
-)(ReadList)
